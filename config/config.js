9fb56b9cce07338d5312e60fb8cf8711b89b661b
Eric Prostko <ericprostko9@gmail.com>
2017-03-18 19:15:29 -0400

    Added conversation.js and config, node_helper.js back

--- config/config.js
+++ config/config.js
@@ -39,12 +39,11 @@ var config = {
           username: "5a6159b6-edb8-4741-9584-140d1f1506f2",
           password: "rnag46J0ue",
           endpoint: "observations"
-
         }
     },
     {
       module: 'conversation'
-    }
+		}
   ]
 
 };
