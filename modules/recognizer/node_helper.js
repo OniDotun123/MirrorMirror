de031cf42d2ce18ddd7e9d5685b7525a278f09ab
Eric Prostko <ericprostko9@gmail.com>
2017-03-19 15:04:21 -0400

    Add structure for taking a selfie, testing to make sure wired correctly

--- modules/recognizer/node_helper.js
+++ modules/recognizer/node_helper.js
@@ -2,4 +2,15 @@ var NodeHelper = require("node_helper");
 
 module.exports = NodeHelper.create({
   start() {};
+  socketNotificationReceived: function(notification) {
+    if (notification === "RECOGNIZER_CONNECTED"){
+      console.log("Recognizer Node Helper initialized, awaiting Activation");
+      return;
+    }
+
+    if(notification === "TAKE_SELFIE") {
+      console.log("Selfie is being taken now")
+
+    }
+	}
 });
--- modules/recognizer/recognizer.js
+++ modules/recognizer/recognizer.js
@@ -1,18 +1,21 @@
 Module.register("recognizer",{
   start() {
+    console.log("Recognizer started");
     this.sendSocketNotification("CONNECT");
     return;
   },
 
   socketNotificationReceived: function(notification) {
+    console.log("Recognizer recieved a notification: " + notification)
+
     if (notification === "RECOGNIZER_CONNECTED"){
       console.log("Recognizer initialized, awaiting Activation");
       return;
     }
 
     if(notification === "login") {
-      
-      sendSocketNotification();
+      sendSocketNotification("TAKE_SELFIE");
+      return;
     }
 	}
 
