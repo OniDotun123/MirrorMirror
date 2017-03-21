94a15435902b867f9c733f038a99f6dfe9ad5a80
Eric Prostko <ericprostko9@gmail.com>
2017-03-21 10:24:59 -0400

    Added multipart form data, hopeful

--- modules/recognizer/node_helper.js
+++ modules/recognizer/node_helper.js
@@ -14,25 +14,25 @@ module.exports = NodeHelper.create({
     }
 
     else if(notification === "TAKE_SELFIE") {
+      console.log("===Selfie is being taken now====");
+      var image = exec("fswebcam --no-banner ./public/webcam_pic.jpg");
       callForMatches();
       this.sendSocketNotification("SELFIE_IS_GO");
   },
 
   callForMatches: function() {
-    console.log("===Selfie is being taken now====");
-    var image = exec("fswebcam --no-banner ./public/webcam_pic.jpg");
-    var img = fs.writeFileSync('./public/webcam_pic.jpg');
-
+    // var img = fs.createReadStream(__dirname + '../../public/webcam_pic.jpg');
+    console.log("Recognizer Node Helper is calling api")
     var options = {
         api_key: "9oOudn2moC5eM-pQwLy_ugUs6rYRT7aj",
         api_secret: "ROglv8QFta3JmGAppEYTpoPY68DjERzX",
-        image_file: img,
+        image_file: fs.createReadStream(__dirname + '../../public/webcam_pic.jpg'),
         outer_id: "mirrormirror"
       };
 
-    var url = 'https://api-us.faceplusplus.com/facepp/v3/search';
+    var url = "https://api-us.faceplusplus.com/facepp/v3/search";
 
-    var response = request.post({url, options}, function(err, res, body) {
+    var response = request.post({url, options}, function(err, httpRes, body) {
         var json = JSON.parse(body);
         console.log(json);
     })
