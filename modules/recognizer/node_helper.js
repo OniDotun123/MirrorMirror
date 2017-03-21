87dcc68957afa3479542b88f22559b2f907f0c9a
Eric Prostko <ericprostko9@gmail.com>
2017-03-21 09:34:31 -0400

    Ran throguh linter, refactored a little

--- modules/recognizer/node_helper.js
+++ modules/recognizer/node_helper.js
@@ -14,27 +14,28 @@ module.exports = NodeHelper.create({
     }
 
     else if(notification === "TAKE_SELFIE") {
-      console.log("===Selfie is being taken now====");
-      var image = exec('fswebcam --no-banner ./public/webcam_pic.jpg');
-      var img = fs.writeFileSync('./public/webcam_pic.jpg')
-
-      var options = {
-          api_key: '9oOudn2moC5eM-pQwLy_ugUs6rYRT7aj',
-          api_secret: 'ROglv8QFta3JmGAppEYTpoPY68DjERzX',
-          image_file: img,
-          outer_id: 'mirrormirror'
-        }
-      }
-      var url = 'https://api-us.faceplusplus.com/facepp/v3/search',
-
-      var response = request.post({url, options}, function(err, res, body) {
-          var json = JSON.parse(body);
-          console.log(json);
-      })
-
-
+      callForMatches();
       this.sendSocketNotification("SELFIE_IS_GO");
-    }
+  },
+
+  callForMatches: function() {
+    console.log("===Selfie is being taken now====");
+    var image = exec("fswebcam --no-banner ./public/webcam_pic.jpg");
+    var img = fs.writeFileSync('./public/webcam_pic.jpg');
+
+    var options = {
+        api_key: "9oOudn2moC5eM-pQwLy_ugUs6rYRT7aj",
+        api_secret: "ROglv8QFta3JmGAppEYTpoPY68DjERzX",
+        image_file: img,
+        outer_id: "mirrormirror"
+      };
+
+    var url = 'https://api-us.faceplusplus.com/facepp/v3/search';
+
+    var response = request.post({url, options}, function(err, res, body) {
+        var json = JSON.parse(body);
+        console.log(json);
+    })
   }
 
 });
