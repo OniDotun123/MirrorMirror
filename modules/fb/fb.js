18d7f6458ede8131f30b64d8ce7297f8db6bf35b
jordan beth <jordanbeth14@gmail.com>
2017-03-21 20:23:18 -0400

    Update fb.js

--- modules/fb/fb.js
+++ modules/fb/fb.js
@@ -4,7 +4,7 @@ Module.register("fb",{
   apikey: '',
   baseurl: 'https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2F',
   page: 'devbootcamp',
-  endPoint: '&tabs=timeline&width=540&height=1500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId',
+  endPoint: '&tabs=timeline&width=540&height=1700&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId',
   scrolling: 'yes',
   frameborder: '20',
   allowTransparency: 'true'
