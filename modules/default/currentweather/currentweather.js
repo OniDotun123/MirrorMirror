d912ab131bc26cda838e6837db91d0d684179acc
Tatiana Abramova <abramovatatian@gmail.com>
2017-03-22 11:07:02 -0400

    Update currentweather.js

--- modules/default/currentweather/currentweather.js
+++ modules/default/currentweather/currentweather.js
@@ -1,7 +1,7 @@
 Module.register("currentweather", {
 
   defaults: {
-
+    location: "NYC, NY"
   },
 
   getHeader: function(){
@@ -33,7 +33,7 @@ Module.register("currentweather", {
 
     var locationDisplay = document.createElement("span");
     locationDisplay.className = "location-display";
-    locationDisplay.innerHTML = this.config.location; // might need to look into getting and setting location.
+    locationDisplay.innerHTML = this.defaults.location; // might need to look into getting and setting location.
     div.appendChild(locationDisplay);
 
     var temperatureDisplay = document.createElement("span")
@@ -68,7 +68,7 @@ Module.register("currentweather", {
           this.temperature = payload['query']['results']['channel']['item']['condition']['temp']; // temp
           this.high = payload['query']['results']['channel']['item']['forecast'][0]['high'];
           this.low = payload['query']['results']['channel']['item']['forecast'][0]['low'];
-          this.low =  payload['query']['results']['channel']['item']['forecast'][0]['text'];
+          this.forecast =  payload['query']['results']['channel']['item']['forecast'][0]['text'];
 
           this.updateDom();
     }
